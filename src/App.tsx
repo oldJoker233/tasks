06518f45f8ea678087baada48e53473263b88c64
Hexuan Jiang <50747956+oldJoker233@users.noreply.github.com>
2022-02-12 16:28:44 -0500

    Task3-HTML and CSS done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import React from "react";
 import "./App.css";
-
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Name:Hexuan Jiang
             </header>
+            <h2>SC2 Zerg Image</h2>
+            <img
+                src="C:/Users/admin/Pictures/Saved Pictures/Zerg.jpg"
+                alt="A picture of Zerg"
+            />
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <div
+                        style={{
+                            border: "1px solid red",
+                            background: "red",
+                            height: "80px",
+                            width: "650px"
+                        }}
+                    >
+                        <Col xs="auto">
+                            <ol>
+                                <li>Maru</li>
+                                <li>Rogue</li>
+                                <li>Zest</li>
+                            </ol>
+                        </Col>
+                    </div>
+                    <div
+                        style={{
+                            border: "1px solid red",
+                            background: "red",
+                            height: "80px",
+                            width: "650px"
+                        }}
+                    >
+                        <Col xs="auto">
+                            On the left side are 3 SC2 pro gammer.
+                        </Col>
+                    </div>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
